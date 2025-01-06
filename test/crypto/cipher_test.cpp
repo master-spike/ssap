a068013196ee79fab8dde3d2f9e3cee14c430a98
Najeeb Al-Shabibi <najeeb.al.shabibi@gmail.com>
2025-01-06 11:50:06 +0000

    Added rsa encryption test for other type

--- test/crypto/cipher_test.cpp
+++ test/crypto/cipher_test.cpp
@@ -1,6 +1,7 @@
 #include "crypto/cipher.hpp"
 
 #include <span>
+#include <variant>
 
 #include <catch2/catch_test_macros.hpp>
 #include <openssl/evp.h>
@@ -41,4 +42,45 @@ TEST_CASE("rsa encrypt-decrypt block") {
     // INFO("decrypted string : " << str_decrypted);
     INFO("decrypted size : " << s.value().size());
     REQUIRE(str_decrypted == sample_str);
+}
+
+namespace detail {
+
+struct foo {
+    std::array<float, 6> f;
+    uint64_t u;
+};
+
+} // namespace detail
+
+TEST_CASE("rsa encrypt-decrypt variant") {
+    std::variant<unsigned, float, char> v;
+
+    MAKE_OSSL_UNIQUE_PTR(EVP_PKEY, pkey, EVP_RSA_gen(2048));
+    REQUIRE(pkey);
+
+    std::unique_ptr<EVP_PKEY_CTX, void (*)(EVP_PKEY_CTX*)> check_ctx(
+        EVP_PKEY_CTX_new_from_pkey(nullptr, pkey.get(), nullptr),
+        EVP_PKEY_CTX_free);
+    REQUIRE(EVP_PKEY_public_check(check_ctx.get()));
+    REQUIRE(EVP_PKEY_private_check(check_ctx.get()));
+
+    std::span<std::variant<unsigned, float, char>> v_span(&v, 1);
+
+    auto r =
+        ssap::crypto::rsa_encrypt_block<std::variant<unsigned, float, char>>(
+            v_span, pkey.get());
+
+    REQUIRE(r.has_value());
+    REQUIRE(r.value().size() > 0);
+
+    auto d = ssap::crypto::rsa_decrypt_block(
+        std::span(r.value().begin(), r.value().end()), pkey.get());
+
+    REQUIRE(d.has_value());
+    REQUIRE(d.value().size() == sizeof(decltype(v)));
+
+    decltype(v) u;
+    std::memcpy(&u, d.value().data(), sizeof(decltype(u)));
+    REQUIRE(v == u);
 }
